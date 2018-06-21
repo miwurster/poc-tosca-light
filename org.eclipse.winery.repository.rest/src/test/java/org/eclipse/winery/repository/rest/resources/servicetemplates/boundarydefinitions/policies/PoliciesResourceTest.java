15e2219af6aaf481eada7656bccdf210ec9a0afe
Vladimir Yussupov <v.yussupov@gmail.com>
2018-06-21 16:37:58 +0200

    Fix tests in repository.rest
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- org.eclipse.winery.repository.rest/src/test/java/org/eclipse/winery/repository/rest/resources/servicetemplates/boundarydefinitions/policies/PoliciesResourceTest.java
+++ org.eclipse.winery.repository.rest/src/test/java/org/eclipse/winery/repository/rest/resources/servicetemplates/boundarydefinitions/policies/PoliciesResourceTest.java
@@ -35,7 +35,7 @@ public class PoliciesResourceTest extends AbstractResourceTest {
     @Test
     public void deletePolicies() throws Exception {
         this.setRevisionTo("3cad4e459a8af6082097eab8f978c80b4f5a512e");
-        this.assertDelete("servicetemplates/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fservicetemplates%252Ffruits/baobab_serviceTemplate/boundarydefinitions/policies/1400281569/");
+        this.assertDelete("servicetemplates/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fservicetemplates%252Ffruits/baobab_serviceTemplate/boundarydefinitions/policies/-926105316/");
         this.assertGet("servicetemplates/http%253A%252F%252Fwinery.opentosca.org%252Ftest%252Fservicetemplates%252Ffruits/baobab_serviceTemplate/boundarydefinitions/policies/", "entitytypes/empty_array.json");
     }
 }
