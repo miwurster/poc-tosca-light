92463fbc4fa4b9f23334c4ccff97468041e0476a
Tobias Mathony <mathony.tobias@gmail.com>
2019-11-25 11:48:39 +0100

    generate multi-participant component

---
+++ org.eclipse.winery.frontends/app/topologymodeler/src/app/multi-participants/multi-participants.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IWineryState } from '../redux/store/winery.store';
+import { TopologyRendererActions } from '../redux/actions/topologyRenderer.actions';
+import { WineryActions } from '../redux/actions/winery.actions';
+import { ToastrService } from 'ngx-toastr';
+import { MultiParticipantsService } from '../services/multi-participants.service';
+import { TopologyRendererState } from '../redux/reducers/topologyRenderer.reducer';
+import { WineryRepositoryConfigurationService } from '../../../../tosca-management/src/app/wineryFeatureToggleModule/WineryRepositoryConfiguration.service';
+import { backendBaseURL } from '../../../../tosca-management/src/app/configuration';
+import { TopologyModelerConfiguration } from '../models/topologyModelerConfiguration';
+import { BackendService } from '../services/backend.service';
+
+@Component({
+    selector: 'winery-multi-participants',
+    templateUrl: './multi-participants.component.html',
+    styleUrls: ['./multi-participants.component.css']
+})
+export class MultiParticipantsComponent implements OnInit {
+
+    readonly uiURL = encodeURIComponent(window.location.origin + window.location.pathname + '#/');
+    private readonly configuration: TopologyModelerConfiguration;
+
+    constructor(private ngRedux: NgRedux<IWineryState>,
+                private actions: TopologyRendererActions,
+                private wineryActions: WineryActions,
+                private alert: ToastrService,
+                private multiParticipantsService: MultiParticipantsService,
+                private wineryConfigurationService: WineryRepositoryConfigurationService,
+                private backendService: BackendService) {
+        this.configuration = backendService.configuration;
+        this.ngRedux.select(state => state.topologyRendererState).subscribe(
+            currentButtonState => this.checkButtonsState(currentButtonState)
+        );
+    }
+
+    /**
+     * This method checks the current button state of Winery UI to take action when the Generate Placeholder Button was
+     * clicked.
+     * @param currentButtonsState TopologyRendererState object containt state of Winery UI Buttons
+     */
+    private checkButtonsState(currentButtonsState: TopologyRendererState) {
+        // check if Generate Placeholder Button is clicked
+        if (currentButtonsState.buttonsState.generatePlaceholder) {
+            this.multiParticipantsService.postNewVersion().subscribe(
+                response => {
+                    console.log(response);
+                    const editorConfig = '?repositoryURL=' + this.configuration.repositoryURL
+                        + '&uiURL=' + encodeURIComponent(backendBaseURL)
+                        + '&ns=' + response.namespace
+                        + '&id=' + response.localname;
+                    window.open(this.wineryConfigurationService.configuration.endpoints.topologymodeler + editorConfig);
+                    this.multiParticipantsService.postPlaceholders(response.localname).subscribe(
+                        data => {
+                            console.log(data);
+                        }
+                    );
+                },
+                err => {
+                    console.log(err);
+                }
+            );
+        }
+    }
+
+    ngOnInit() {
+    }
+
+}
